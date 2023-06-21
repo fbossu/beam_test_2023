0ed8c572ee155bc60b8cc7343db07029954ce5e3
Francesco Bossù <fbossu@jlab.org>
2023-06-21 17:00:16 +0200

    example

--- examples/timeVsStrip.C
+++ examples/timeVsStrip.C
@@ -17,6 +17,7 @@ struct cluster {
 void timeVsStrip() {
 
 
+  int dreamID = 7;
   TFile *inf = TFile::Open( "rec_ftest_POS13_FEU4.root" );
 
   TTreeReader reader("events", inf);
@@ -24,9 +25,16 @@ void timeVsStrip() {
   TTreeReaderValue< std::vector<hit> > hits( reader, "hits");
   TTreeReaderValue< std::vector<cluster> > cls( reader, "clusters");
 
-  int dreamID = 7;
 
   TH1F *hcentroid = new TH1F("hcentroid", "centroid", 4*64,0,64);
+  hcentroid->SetXTitle("centroid");
+  TH2F *hsize = new TH2F("hsize", "size", 4*64,0,64, 20,0,20);
+  hsize->SetXTitle("centroid");
+  hsize->SetYTitle("cluster size");
+  TH2F *h2c = new TH2F("h2c", "centroid", 4*64,0,64,4*64,0,64);
+  h2c->SetXTitle("centroid");
+  h2c->SetYTitle("centroid");
+
 
   while( reader.Next() ){
 
@@ -36,11 +44,34 @@ void timeVsStrip() {
       if( floor( c.centroid/64 ) != dreamID ) continue;
 
       hcentroid->Fill( c.centroid - dreamID*64);
+      hsize->Fill( c.centroid - dreamID*64, c.size);
+
     }
+
+    if( cls->size() > 1){
+      for( auto icl = cls->begin(); icl < cls->end(); icl++){
+        if( floor( icl->centroid/64 ) != dreamID ) continue;
+        for( auto jcl = icl + 1; jcl < cls->end(); jcl++ ){
+          if( floor( jcl->centroid/64 ) != dreamID ) continue;
+          h2c->Fill( icl->centroid - dreamID*64, jcl->centroid - dreamID*64 );
+        }
+      }
+
+    }
+
 
   }
 
+  auto c = new TCanvas();
+  c->Divide(2,2);
+  c->cd(1);
   hcentroid->Draw();
+  c->cd(2)->SetLogz();
+  h2c->Draw("colz");
+  c->cd(3)->SetLogz();
+  hsize->Draw("colz");
+  c->cd(4)->SetLogz();
+  hsize->ProjectionY()->Draw();
 
 
 }
