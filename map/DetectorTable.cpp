36fca9cf0bcdc5447c32e385eff0a7e98b16da9a
samy polcherrafael <sp270081@dphnpct77.extra.cea.fr>
2023-06-25 14:06:23 +0200

    test invert feu connections

--- map/DetectorTable.cpp
+++ map/DetectorTable.cpp
@@ -106,7 +106,7 @@ std::vector<int> DetectorTable::getNeighbours(int channel){
 
 std::string DetectorTable::getAll(int channel){
 	std::string out = "ch: " + std::to_string(channel) + " cnt: " + std::to_string(this->getConnector(channel)) + " cntChannel: " + 
-		std::to_string(dreamConnect[channel - this->getConnector(channel)]*64) + " axis: " + std::to_string(this->getPitch(channel)) +
+		std::to_string(channel - dreamConnect[this->getConnector(channel)]*64) + " axis: " + this->getPitch(channel) +
 		" pitch: " + std::to_string(this->getPitch(channel));
 	return out;
 }
