e20ed9b795405094b9ff49f8d983c8ac6ec7f9ea
samy polcherrafael <sp270081@dphnpct77.extra.cea.fr>
2023-06-26 16:38:47 +0200

    test invert feu connections

--- reco/reco.cpp
+++ reco/reco.cpp
@@ -275,7 +275,7 @@ int main( int argc, char **argv ){
   }
   else if(nbFeu == 4){
     det = DetectorTable("../map/asa_map.txt", 4, 5, 6, 7);
-    det.setInversion(true, true, false, false);
+    det.setInversion(false, false, false, true);
   }
   else if(nbFeu == 5) {cerr << "P2 map not yet implemented \n"; return 1;}
   else {cerr << "Feu number is invalid \n"; return 1;}
