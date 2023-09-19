05eadd672a61e6dc686c9633c76b923a1b10c238
Francesco Bossù <fbossu@jlab.org>
2023-09-19 16:35:12 +0200

    reading multiple files

--- banco/reco_banco.cpp
+++ banco/reco_banco.cpp
@@ -1,5 +1,6 @@
 #include "TFile.h"
 #include "TTree.h"
+#include "TChain.h"
 #include "TTreeReader.h"
 #include "definition_banco.h"
 
@@ -12,12 +13,14 @@
 #include "Math/Rotation3D.h"
 #include "Math/Vector3D.h"
 
+#include "utils.h"
+
 using namespace ROOT::Math;
 
 namespace banco {
   class Ladder {
     public:
-      Ladder() { pitch = 0.040; } // 40µm
+      Ladder() { pitch = 0.025; } // 25µm
       virtual ~Ladder() {};
       
       void CentroidToLocal( float, float, XYZVector * ); // col (x), row (y)
@@ -26,53 +29,122 @@ namespace banco {
 
       void SetRotation( Rotation3D r ){ rotation = r; }
       void SetTranslation( XYZVectorF t ){ translation = t; }
+      void SetReference( XYZVectorF r ){ reference = r; }
 
     private:
       float pitch;
       Rotation3D rotation;
       XYZVectorF translation;
+      XYZVectorF reference;
 
   };
 
+  struct positions {
+    std::vector<XYZVector>  ref;
+    std::vector<XYZVector>  pos;
+    std::vector<Rotation3D> rot;
+  };
 };
+
 void banco::Ladder::CentroidToLocal( float col, float row, XYZVector *pos ){
   pos->SetXYZ( col * pitch, row * pitch, 0. );
 }
 
 void banco::Ladder::LocalToGlobal( XYZVector *pos ){
+  *pos = (*pos) - reference ;
   *pos = rotation*(*pos);
   *pos = translation + (*pos);
 }
 
 // =================================================================
 
-void recoBanco(std::string fnameIn, std::string fnameOut){
+void recoBanco(std::vector<std::string> fnamesIn){
 
-	TFile* fin = TFile::Open(fnameIn.c_str(),"read");
-	TTreeReader reader("events",fin);
-	TTreeReaderValue<std::vector<banco::cluster>> cls(reader, "clusters");
+  // chain the N ladders together
+  TTree *tree = 0x0;
 
-  banco::Ladder lad;
-  lad.SetRotation( Rotation3D(0,1,0,-1,0,0,0,0,1) );
-  lad.SetTranslation( XYZVectorF(0,0,10) );
-  lad.SetPitch( 10 );
-  XYZVector v;
+  std::vector<std::string> tnames;
+
+  for( auto f : fnamesIn ){
+	  TFile* fin = TFile::Open(f.c_str(),"read");
+    TTree *tin = fin->Get<TTree>("events");
+    auto lid = f.substr( f.find( "ladder"), 6 + 3 );
+    std::cout << lid << "\n";
+   
+    tnames.push_back( lid );
+
+    if( ! tree ) {
+      tree = tin;
+      tree->SetName( lid.c_str() );
+    }
+    else 
+      tree->AddFriend( tin, lid.c_str() );
+    
+  }
+	TTreeReader reader( tree );
+
+
+  // link the cluster branches for the different ladders
+  // use a map for accessing them by name "ladder#"
+  std::map< std::string, TTreeReaderValue<std::vector<banco::cluster>>* > cls;
+  for( auto s : tnames ){
+    TTreeReaderValue<std::vector<banco::cluster>> *r = new TTreeReaderValue<std::vector<banco::cluster>>(reader, (s+".clusters").c_str() );
+    cls[s] = r;
+  }
+
+  // geometry
+  // --------
+  std::map< std::string, banco::Ladder > geom;
+  for( auto s : tnames ){
+    geom[s];
+    // TODO: read conf from file
+    //geom[s].SetRotation( Rotation3D(0,1,0,-1,0,0,0,0,1) );
+    //geom[s].SetTranslation( XYZVectorF(0,0,10) );
+  }
+
+
+  // some plots
+  // ----------
+  axis *acx = createAxis( "centroid x", 4000, 0, 128. ); 
+  axis *acy = createAxis( "centroid y", 400, 0, 12.8 );
+
+  TH2F *hcorx = create2DHisto( "hcorx", "corr x", acx, acx );
+  TH2F *hcory = create2DHisto( "hcory", "corr y", acy, acy );
+
+  // loop over events
+  // ================
   int i=0;
   while( reader.Next() && i<10 ){
-    i++;
-    for( auto cl : *cls ){
-      lad.CentroidToLocal( cl.colCentroid, cl.rowCentroid, &v );
-      lad.LocalToGlobal( &v );
-      std::cout << v << std::endl;
+    //i++;
+    XYZVector c0;
+    XYZVector c1;
+    //std::cout << (*cls[tnames[0]])->size() << " " << (*cls[tnames[1]])->size() << "\n";
+    for( auto cls0 = (*cls[tnames[0]])->begin(); cls0 < (*cls[tnames[0]])->end(); cls0++ ){
+      geom[tnames[0]].CentroidToLocal( cls0->colCentroid, cls0->rowCentroid, &c0);
+      for( auto cls1 = (*cls[tnames[1]])->begin(); cls1 < (*cls[tnames[1]])->end(); cls1++ ){
+        geom[tnames[1]].CentroidToLocal( cls1->colCentroid, cls1->rowCentroid, &c1);
+        
+        //std::cout <<  c0.X() << " " <<  c1.X() << "\n";
+        //std::cout <<  c0.Y() << " " <<  c1.Y() << "\n";
+        hcorx->Fill( c0.X(), c1.X() );
+        hcory->Fill( c0.Y(), c1.Y() );
+      }
     }
+    
   }
+  hcorx->SaveAs("hcorx.root");
+  hcory->SaveAs("hcory.root");
 
 }
 
 int main(int argc, char const *argv[])
 {
-	/* code */
-	std::string fnameIn = argv[1];
-  recoBanco(fnameIn, "" );
+  std::vector<std::string> fnames;
+  for( int i=1; i<argc; i++){
+	  std::string fnameIn = argv[i];
+    std::cout << fnameIn << std::endl;
+    fnames.push_back( fnameIn );
+  }
+  recoBanco(fnames);
 	return 0;
 }
