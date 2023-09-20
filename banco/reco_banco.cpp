2ab2bfac5ba73a7a8540d818354f38a28646c37c
Francesco Bossù <fbossu@jlab.org>
2023-09-20 11:02:00 +0200

    residuals

--- banco/reco_banco.cpp
+++ banco/reco_banco.cpp
@@ -55,6 +55,24 @@ banco::track Fit( std::vector<XYZVector> seed ){
   return tr;
 }
 
+void Residuals( banco::track trk, XYZVector p, TH1F *hres, char ax = 'x' ){
+  float z = p.Z();
+  float c = 0.;
+  float m = 0.;
+  float r = 0.;
+  if( ax == 'x' ){
+    c = trk.x0;
+    m = trk.mx;
+    r = ( c + m*z ) - p.X();
+  }
+  else {
+    c = trk.y0;
+    m = trk.my;
+    r = ( c + m*z ) - p.Y();
+  }
+  hres->Fill( r );
+}
+
 // =================================================================
 
 void recoBanco(std::vector<std::string> fnamesIn){
@@ -120,10 +138,19 @@ void recoBanco(std::vector<std::string> fnamesIn){
   axis *acy = createAxis( "centroid y", 2000, 0, 128. ); 
   axis *acx = createAxis( "centroid x", 200, 0, 12.8 );
 
+  axis *arescx = createAxis( "residual x", 300, -1.2, 1.2 ); 
+  axis *arescy = createAxis( "residual y", 300, -1.2, 1.2 ); 
+
   std::map<std::string, TH2F*> mh2xy;
+  std::map<std::string, TH1F*> mhresx;
+  std::map<std::string, TH1F*> mhresy;
   for( auto s : tnames ){
     mh2xy[s] = create2DHisto( Form("h2xy_%s",s.c_str()), Form("xy %s",s.c_str()), acx, acy );
     mh2xy[s]->SetDirectory(hdir);
+    mhresx[s] = createHisto( Form("hresx_%s",s.c_str()), Form("res x %s",s.c_str()), arescx );
+    mhresx[s]->SetDirectory(hdir);
+    mhresy[s] = createHisto( Form("hresy_%s",s.c_str()), Form("res y %s",s.c_str()), arescy );
+    mhresy[s]->SetDirectory(hdir);
   }
   TH2F *hcorx = create2DHisto( "hcorx", "corr x", acx, acx );
   hcorx->SetDirectory(hdir);
@@ -133,8 +160,8 @@ void recoBanco(std::vector<std::string> fnamesIn){
   // loop over events
   // ================
   int i=0;
-  while( reader.Next() && i<10 ){
-    //i++;
+  while( reader.Next() && i<5e4 ){
+    i++;
     trEvId = *eventId;
     //fill some histos
     for( auto s : tnames ){
@@ -159,6 +186,7 @@ void recoBanco(std::vector<std::string> fnamesIn){
     // find combinations of clusters and consider them as a seed
     //std::vector< std::vector<XYZVector> > lseeds;
     std::vector<XYZVector> seed;
+    std::vector<std::string> seeddet;
 
     // loop over the ladders
     for( auto icl : cls ){
@@ -166,10 +194,19 @@ void recoBanco(std::vector<std::string> fnamesIn){
       geom[icl.first].CentroidToLocal( (*icl.second)->at(0), &p ); // TODO check if there are any cls
       geom[icl.first].LocalToGlobal(&p);
       seed.push_back(p);
+      seeddet.push_back(icl.first);// maybe not useful if detector are ordered
     }
 
     auto track = Fit( seed );
     tracks->push_back(track);
+
+    for( auto trk : *tracks ){
+        for( int i=0;i<seed.size(); i++){
+          Residuals( trk, seed[i], mhresx[seeddet.at(i)], 'x');
+          Residuals( trk, seed[i], mhresy[seeddet.at(i)], 'y');
+        }
+    }
+
     nt->Fill();
     tracks->clear();
   }
