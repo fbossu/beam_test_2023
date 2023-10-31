aefb65e820c5e621aed2425035621426cbea5953
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2023-10-31 23:32:52 +0100

    added clampplots

--- analysis/clusterSizeStrip.cpp
+++ analysis/clusterSizeStrip.cpp
@@ -21,28 +21,39 @@ int main(int argc, char const *argv[])
   basedir = basedir.substr(0, basedir.find_last_of("/")) + "/";
   std::cout << basedir << std::endl;
 
-  TChain* chain = new TChain("events");
+  StripTable det(basedir+"../map/strip_map.txt");
+
+  // TChain* chain = new TChain("events");
   std::string detName = "test";
   for( int i = 1; i < argc; i++) {
 
     TString input = argv[i];
 
     if( input.Contains( "root" ) ){
-      chain->Add( input );
+      // chain->Add( input );
     }
     else{
       std::cout<<"Detector Name: "<<argv[i]<<std::endl;
       detName = argv[i];
     }
   }
-  StripTable det(basedir+"../map/strip_map.txt");
-  clusterSizeRegion(chain, detName, det);
-  clusterSizeLims(chain, detName, det, {55, 62}, {30, 50}); //1, 1
-  clusterSizeLims(chain, detName, det, {70, 85}, {30, 50}); //1, 1.5
-  clusterSizeLims(chain, detName, det, {70, 85}, {75, 90}); //1.5, 1.5
-  clusterSizeLims(chain, detName, det, {55, 62}, {75, 90}); //1.5, 1.
-  clusterSizeLims(chain, detName, det, {95, 105}, {75, 90}); //1.5, 0.5
-  clusterSizeLims(chain, detName, det, {95, 105}, {30, 50}); //1., 0.5
+
+    for( int i = 2; i < argc; i++) {
+    std::string fname = argv[i];
+    int pos = std::stoi( fname.substr(fname.find("POS")+3, fname.find("POS")+5) );
+    if(std::find(zoneRuns.begin(), zoneRuns.end(), pos) != zoneRuns.end()){
+      // clusterSizeLims(chain, detName, det, fname);
+      clSize_Amp(fname, detName, det); 
+    }
+  }
+
+  // clusterSizeRegion(chain, detName, det);
+  // clusterSizeLims(chain, detName, det, {55, 62}, {30, 50}); //1, 1
+  // clusterSizeLims(chain, detName, det, {70, 85}, {30, 50}); //1, 1.5
+  // clusterSizeLims(chain, detName, det, {70, 85}, {75, 90}); //1.5, 1.5
+  // clusterSizeLims(chain, detName, det, {55, 62}, {75, 90}); //1.5, 1.
+  // clusterSizeLims(chain, detName, det, {95, 105}, {75, 90}); //1.5, 0.5
+  // clusterSizeLims(chain, detName, det, {95, 105}, {30, 50}); //1., 0.5
 
 
   return 0;
