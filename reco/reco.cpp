9d465a2fe2a0c41f505e58a8046bebb8fb296667
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2023-12-09 19:10:05 +0100

    chaint strip

--- reco/reco.cpp
+++ reco/reco.cpp
@@ -308,7 +308,8 @@ int main( int argc, char **argv ){
     if(nbDet == 1){
       // det = DreamTable(basedir + "../map/strip_map.txt", 0, 1, 2, 3);
       det = DreamTable(basedir + "../map/strip_map.txt", 3, 2, 1, 0);
-      det.setInversion(true, true, false, true);
+      // det.setInversion(true, true, false, true);
+      det.setInversion(false, false, true, false);
     }
     else if(nbDet == 2){
       det = DreamTable(basedir + "../map/inter_map.txt", 4, 5, 6, 7);
