9c6671803a0409700daf441b70e24f1296ddb3be
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2023-12-11 11:30:12 +0100

    reco test

--- reco/reco.cpp
+++ reco/reco.cpp
@@ -308,7 +308,8 @@ int main( int argc, char **argv ){
     if(nbDet == 1){
       // det = DreamTable(basedir + "../map/strip_map.txt", 1, 0, 2, 3);
       det = DreamTable(basedir + "../map/strip_map.txt", 2, 3, 0, 1);
-      det.setInversion(true, true, false, true);
+      // det.setInversion(true, true, false, true);
+      det.setInversion(false, true, true, true);
       // det = DreamTable(basedir + "../map/strip_map.txt", atoi(argv[4]), atoi(argv[5]), atoi(argv[6]), atoi(argv[7]));
       // printf("det %d %d %d %d \n", atoi(argv[4]), atoi(argv[5]), atoi(argv[6]), atoi(argv[7]));
       // det.setInversion(true, true, true, true);
