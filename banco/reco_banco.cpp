20ce8725b4ed9fe59a88532978f7da35630b0e6a
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2024-01-14 01:34:11 +0100

    test reco banco

--- banco/reco_banco.cpp
+++ banco/reco_banco.cpp
@@ -136,7 +136,7 @@ std::string geofname = "geometries.txt";
 std::string rotfname = "global_rotation.txt";
 int NEVENTS = -1;
 bool DoRES = false;
-float MaxR = 10.;
+float MaxR = 1.;
 
 // =================================================================
 
