7828080b16bf4fc7d633d8a11b5d659611bcc180
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2024-01-18 16:02:36 +0100

    efficiency 3sigma

--- analysisNew/plot_tables.py
+++ analysisNew/plot_tables.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from math import sqrt
 import numpy as np
 from matplotlib.lines import Line2D
+from matplotlib.ticker import MaxNLocator
 
 
 # function to parse the folowing file format:
@@ -42,47 +43,53 @@ def parse_file(file_path, msc):
             i += 1
     return Gdata, Xdata, Ydata
 
-plt.rcParams.update({'font.size': 18})
+plt.rcParams.update({'font.size': 26})
+plt.rcParams.update({'lines.markersize': 26})
+
 # listmsc = [0.101, 0.212, 0.364, 0.549, 0.766]
 listmsc = [0.101, 0.197, 0.323, 0.461, 0.600]
 
 file_pathStrip = './stripFEU1Inv/stripFEU1_table.txt'
 GdataStrip, XdataStrip, YdataStrip = parse_file(file_pathStrip, 0)
+# GdataStrip, XdataStrip, YdataStrip = parse_file(file_pathStrip, listmsc[0])
 
 file_pathInter = './interFEU1Inv/interFEU1_table.txt'
 GdataInter, XdataInter, YdataInter = parse_file(file_pathInter, 0)
+# GdataInter, XdataInter, YdataInter = parse_file(file_pathInter, listmsc[1])
 
 file_pathPlein = './asaFEU2Inv/asaFEU2_table.txt'
 GdataPlein, XdataPlein, YdataPlein = parse_file(file_pathPlein, 0)
+# GdataPlein, XdataPlein, YdataPlein = parse_file(file_pathPlein, listmsc[2])
 
 file_pathAsa = './asaFEU4Inv/asaFEU4_table.txt'
 GdataAsa, XdataAsa, YdataAsa = parse_file(file_pathAsa, 0)
+# GdataAsa, XdataAsa, YdataAsa = parse_file(file_pathAsa, listmsc[4])
 
 
 def plotPitch(XdataStrip, YdataStrip, XdataInter, YdataInter, val, yTitle, graphName):
     fig = plt.figure( figsize=(10, 8) )
-    fig.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
+    # plt.tight_layout()
+    fig.subplots_adjust(left=0.135, right=0.98, bottom=0.11, top=0.88)
     ax1 = fig.add_subplot(111)
     ax2 = ax1.twiny()
 
-    Xtick_locations = [0.4, 0.5, 0.6, 0.9, 1., 1.1, 1.4, 1.5, 1.6]
+    tick_locations = [0.38, 0.5, 0.62, 0.87, 1., 1.12, 1.38, 1.5, 1.62]
     Xtick_labels = [0.5, 0.5, 0.5, 1., 1., 1., 1.5, 1.5, 1.5 ]
 
-    Ytick_locations = [0.4, 0.5, 0.6, 0.9, 1., 1.1, 1.4, 1.5, 1.6]
     Ytick_labels = [0.5, 1, 1.5, 0.5, 1, 1.5, 0.5, 1, 1.5 ]
 
-    ax1.set_xticks(Xtick_locations)
+    ax1.set_xticks(tick_locations)
     ax1.set_xticklabels(Xtick_labels)
-    ax2.set_xticks(Ytick_locations)
+    ax2.set_xticks(tick_locations)
     ax2.set_xticklabels(Ytick_labels)
 
     for i, x in enumerate(XdataStrip):
         xD = x[0]
         if(YdataStrip[i][0]==0.5):
-            xD = xD - 0.1
+            xD = xD - 0.12
         elif(YdataStrip[i][0]==1.5):
-            xD = xD + 0.1
-        if val==4:
+            xD = xD + 0.12
+        if val==40:
             plt.errorbar(xD, x[val], xerr=0.0, yerr=x[5], fmt='b.', markersize=15, capsize=5,    label='stripFEU1 Xstrips, inter = 0.1 ')
         else :
             plt.plot(xD, x[val], 'b.', markersize=15, label='stripFEU1 Xstrips, inter = 0.1 ')
@@ -91,23 +98,23 @@ def plotPitch(XdataStrip, YdataStrip, XdataInter, YdataInter, val, yTitle, graph
     for i, x in enumerate(YdataStrip):
         xD = x[0]
         if(XdataStrip[i][0]==0.5):
-            xD = xD - 0.1
+            xD = xD - 0.12
         elif(XdataStrip[i][0]==1.5):
-            xD = xD + 0.1
-        if val==4:
+            xD = xD + 0.12
+        if val==40:
             plt.errorbar(xD, x[val], xerr=0.0, yerr=x[5], fmt='r.', markersize=15, capsize=5,    label='stripFEU1 Ystrips, inter = 0.75')
         else :
             plt.plot(xD, x[val], 'r.', markersize=15, label='stripFEU1 Ystrips, inter = 0.75')
 
     for i, x in enumerate(XdataInter):
-        if val==4:
+        if val==40:
             plt.errorbar(x[0], x[val], xerr=0.0, yerr=x[5], fmt='b^', markersize=15, capsize=5,  label='interFEU1 Xstrips, inter = 0.1 ')
         else :
             plt.plot(x[0], x[val], 'b^', markersize=15, label='interFEU1 Xstrips, inter = 0.1 ')
         # plt.annotate(f'{YdataInter[i][0]}', (x[0], x[4]))
 
     for i, x in enumerate(YdataInter):
-        if val==4:
+        if val==40:
             plt.errorbar(x[0], x[val], xerr=0.0, yerr=x[5], fmt='r^', markersize=15, capsize=5,  label='interFEU1 Ystrips, inter = 0.5 ')
         else :
             plt.plot(x[0], x[val], 'r^', markersize=15, label='interFEU1 Ystrips, inter = 0.5 ')
@@ -120,21 +127,23 @@ def plotPitch(XdataStrip, YdataStrip, XdataInter, YdataInter, val, yTitle, graph
                 Line2D([0], [0], color='r', lw=0, linestyle='-', marker='^', markersize=15)]
 
     # Define the labels
-    labels = ['D1 X, strip=100% pitch', 'D1 Y, strip=25% pitch',
-              'D2 X, strip=100% pitch', 'D2 Y, strip=50% pitch']
+    labels = ['D1 B, strip=pitch', 'D1 T, strip=25%pitch',
+              'D2 B, strip=pitch', 'D2 T, strip=50%pitch']
 
     # Create the legend
-    plt.legend(handles=line_styles, labels=labels, fontsize=16)
+    plt.legend(handles=line_styles, labels=labels, borderaxespad=0.07, fontsize=20)
 
     ax1.set_xlim(0.3, 1.7)
     ax2.set_xlim(ax1.get_xlim())
     ax1.set_xlabel('Pitch [mm]')
 
+    # ax1.yaxis.set_major_locator(MaxNLocator(nbins=10))
     if(val==2) : ax1.set_ylim(1.15, 3.1)
-    if(val==3) : ax1.set_ylim(0.3, 0.7)
+    if(val==3) : ax1.set_ylim(0.2, 0.8)
 
     if val == 4:
         ax1.set_ylabel(yTitle+' [mm]')
+        ax1.set_ylim(0.13, 0.4)
     else :
         ax1.set_ylabel(yTitle)
 
@@ -144,14 +153,15 @@ def plotPitch(XdataStrip, YdataStrip, XdataInter, YdataInter, val, yTitle, graph
         ax1.spines[side].set_linewidth(2)
         ax2.spines[side].set_linewidth(2)
 
-    plt.title(yTitle+' vs Pitch')
+    plt.title(yTitle+' vs Pitch', loc='center')
+    # fig.suptitle(yTitle+' vs Pitch', x=0.5, y=0.98)
     # plt.show()
     fig.savefig(graphName)
 
 
 def plotPitchAsa(XdataStrip, YdataStrip, XdataPlein, YdataPlein, val, yTitle, graphName):
     fig = plt.figure( figsize=(10, 8) )
-    fig.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
+    fig.subplots_adjust(left=0.135, right=0.98, bottom=0.11, top=0.88)
     ax1 = fig.add_subplot(111)
 
     Xtick_locations = [0.8, 1., 1.5, 2.]
@@ -166,27 +176,27 @@ def plotPitchAsa(XdataStrip, YdataStrip, XdataPlein, YdataPlein, val, yTitle, gr
     # plt.plot([x[0] for x in YdataInter], [x[4] for x in XdataInter], 'b^', label='interFEU1 Xstrips, inter = 0.1 ', markersize=15)
     # plt.plot([x[0] for x in YdataInter], [x[4] for x in YdataInter], 'r^', label='interFEU1 Ystrips, inter = 0.5 ', markersize=15)
     for i, x in enumerate(XdataStrip):
-        if val==4:
+        if val==40:
             plt.errorbar(x[0], x[val], xerr=0.0, yerr=x[5], fmt='k.', markersize=15, capsize=5)
         else :
             plt.plot(x[0], x[val], 'k.', markersize=15)
         # plt.annotate(f'{YdataStrip[i][0]}', (x[0], x[4]))
 
     for i, x in enumerate(YdataStrip):
-        if val==4:
+        if val==40:
             plt.errorbar(x[0], x[val], xerr=0.0, yerr=x[5], fmt='m.', markersize=15, capsize=5)
         else :
             plt.plot(x[0], x[val], 'm.', markersize=15)
 
     for i, x in enumerate(XdataPlein):
-        if val==4:
+        if val==40:
             plt.errorbar(x[0], x[val], xerr=0.0, yerr=x[5], fmt='k^', markersize=15, capsize=5)
         else :
             plt.plot(x[0], x[val], 'k^', markersize=15)
         # plt.annotate(f'{YdataInter[i][0]}', (x[0], x[4]))
 
     for i, x in enumerate(YdataPlein):
-        if val==4:
+        if val==40:
             plt.errorbar(x[0], x[val], xerr=0.0, yerr=x[5], fmt='m^', markersize=15, capsize=5)
         else :
             plt.plot(x[0], x[val], 'm^', markersize=15)
@@ -203,7 +213,7 @@ def plotPitchAsa(XdataStrip, YdataStrip, XdataPlein, YdataPlein, val, yTitle, gr
             'D4 X, resist full', 'D4 Y, resist full']
 
     # Create the legend
-    plt.legend(handles=line_styles, labels=labels, fontsize=16)
+    plt.legend(handles=line_styles, labels=labels, borderaxespad=0.07, fontsize=20)
 
     ax1.set_xlim(0.5, 2.2)
     ax1.set_xlabel('Pitch [mm]')
@@ -211,6 +221,7 @@ def plotPitchAsa(XdataStrip, YdataStrip, XdataPlein, YdataPlein, val, yTitle, gr
     for side in ['bottom', 'right', 'top', 'left']:
         ax1.spines[side].set_linewidth(2)
     
+    # ax1.yaxis.set_major_locator(MaxNLocator(nbins=10))
     if(val==2) : ax1.set_ylim(1.15, 3.1)
     if(val==3) : ax1.set_ylim(0.3, 0.7)
 
@@ -220,7 +231,8 @@ def plotPitchAsa(XdataStrip, YdataStrip, XdataPlein, YdataPlein, val, yTitle, gr
         ax1.set_ylabel(yTitle)
 
     # fig.legend()
-    plt.title(yTitle+' vs Pitch')
+    plt.title(yTitle+' vs Pitch', loc='center')
+    # fig.suptitle(yTitle+' vs Pitch', x=0.51, y=0.98)
     # plt.show()
     fig.savefig(graphName)
 
@@ -231,3 +243,21 @@ plotPitch(XdataStrip, YdataStrip, XdataInter, YdataInter, 3, 'Amplitude fraction
 plotPitchAsa(XdataAsa, YdataAsa, XdataPlein, YdataPlein, 4, 'Residues', 'Residues_vs_Pitch_Asa.png')
 plotPitchAsa(XdataAsa, YdataAsa, XdataPlein, YdataPlein, 2, 'Cluster size', 'clsize_vs_Pitch_Asa.png')
 plotPitchAsa(XdataAsa, YdataAsa, XdataPlein, YdataPlein, 3, 'Amplitude fraction', 'amp_vs_Pitch_Asa.png')
+
+
+#plot gain of all detectors
+fig = plt.figure( figsize=(10, 8) )
+fig.subplots_adjust(left=0.2, right=0.98, bottom=0.11, top=0.88)
+ax1 = fig.add_subplot(111)
+Xtick_locations = [1, 2., 3, 4.]
+Xtick_labels = ['stripFEU1', 'interFEU1', 'asaFEU4', 'asaFEU2']
+
+ax1.set_xticks(Xtick_locations)
+ax1.set_xticklabels(Xtick_labels)
+
+plt.plot([x[0] for x in GdataStrip], [x[2] for x in GdataStrip], 'b.', label='stripFEU1', markersize=15)
+plt.plot([x[0] for x in GdataInter], [x[2] for x in GdataInter], 'r.', label='interFEU1', markersize=15)
+plt.plot([x[0] for x in GdataAsa], [x[2] for x in GdataAsa], 'g.', label='asaFEU4', markersize=15)
+plt.plot([x[0] for x in GdataPlein], [x[2] for x in GdataPlein], 'k.', label='asaFEU2', markersize=15)
+
+plt.savefig('gain_vs_detector.png')
