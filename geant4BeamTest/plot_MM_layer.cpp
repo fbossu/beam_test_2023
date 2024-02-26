31015f3c3c4cf28e351a91a96987d5777047ad62
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2024-02-26 12:48:33 +0100

    fixed memory leak

--- geant4BeamTest/plot_MM_layer.cpp
+++ geant4BeamTest/plot_MM_layer.cpp
@@ -596,8 +596,8 @@ int main(int argc, char const *argv[])
 { 
   defStyle();
   std::string fname = argv[1];
-  std::vector<double> pitch = {1., 1.5};       // mm
-  std::vector<double> inter =  {0.25};         // fraction of pitch
+  std::vector<double> pitch = {0.5, 1., 1.5};       // mm
+  std::vector<double> inter =  {0.25, 0.5};         // fraction of pitch
   std::vector<double> thRatio = {0.1, 0.2};
   std::vector<double> sigma = {0.2, 0.300, 0.4}; // mm
 
