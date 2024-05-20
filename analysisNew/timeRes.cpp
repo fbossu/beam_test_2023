db15cdfa0c4dc3830f87866c02d99fc5a95be4cc
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2024-05-20 19:21:16 +0200

    added time res code

--- analysisNew/timeRes.cpp
+++ analysisNew/timeRes.cpp
@@ -39,17 +39,36 @@ int main(int argc, char* argv[]) {
             auto hX = getHits(*hits, clX->id);
             h_timeofmaxX->Fill(hX[0].timeofmax);
         }
-        if(clY){
+        else if(clY){
             auto hY = getHits(*hits, clY->id);
             h_timeofmaxY->Fill(hY[0].timeofmax);
         }
+        else{
+            std::vector<hit> hX, hY;
+            for(auto h : *hits){
+                if(h.axis == 'x') hX.push_back(h);
+                else hY.push_back(h);
+                std::sort (hX.begin(), X.end(),
+                    [](const hit& a, const hit& b) {return a.maxamp > b.maxamp;});
+                std::sort (hY.begin(), Y.end(),
+                    [](const hit& a, const hit& b) {return a.maxamp > b.maxamp;});
+                h_timeofmaxX->Fill(hX[0].timeofmax);
+                h_timeofmaxY->Fill(hY[0].timeofmax);
+            }
+        }
     }
+
+    // fit each histogram with a gaussian centered around 4
+    TF1 *f = new TF1("f", "gaus", 0, 10);
+    f->SetParameter(1, 4);
 
     TCanvas *c = new TCanvas("c", "c", 1600, 1200);
     c->Divide(2, 1);
     c->cd(1);
+    h_timeofmaxX->Fit("f");
     h_timeofmaxX->Draw();
     c->cd(2);
+    h_timeofmaxY->Fit("f");
     h_timeofmaxY->Draw();
     c->SaveAs((detName + "_timeofmax.png").c_str());
     return 0;
