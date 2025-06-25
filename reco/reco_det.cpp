cefe325b6984970a3a06450d6acbedc7b56f1e2f
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2025-06-25 10:21:41 +0200

    test inter data Dylan

--- reco/reco_det.cpp
+++ reco/reco_det.cpp
@@ -280,7 +280,7 @@ int main( int argc, char **argv ){
   }
   DreamTable det;
 
-  det = DreamTable(basedir + "../map/strip_map.txt", 4, 5, 6, 7);
+  det = DreamTable(basedir + "../map/inter_map.txt", 1, 2, 3, 74);
   det.setInversion(false, false, false, false);
 
   reco( ch, det );
