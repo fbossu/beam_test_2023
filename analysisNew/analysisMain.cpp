db5775b7cc06e235c027a7c59b87554f61771804
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2025-06-28 17:14:59 +0200

    added mainCospic

--- analysisNew/analysisMain.cpp
+++ analysisNew/analysisMain.cpp
@@ -99,7 +99,7 @@ int main(int argc, char* argv[]) {
         detMap = "../map/strip_map.txt";
     }
     else if (detName.find("inter") != std::string::npos){
-        zoneRuns = { {1,"POS13"}, {0,"POS11"}, {3,"POS06"}, {2,"POS08"} };
+        zoneRuns = { {1,"POS13"}, {0,"POS12"}, {3,"POS06"}, {2,"POS08"} };
         detMap = "../map/inter_map.txt";
     }
     else if (detName.find("asa") != std::string::npos){
