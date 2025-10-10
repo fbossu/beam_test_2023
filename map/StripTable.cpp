881086fb858e52cae13cc711d8e2cbaae611f42f
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2025-10-10 19:19:19 +0200

    json

--- map/StripTable.cpp
+++ map/StripTable.cpp
@@ -14,8 +14,15 @@ StripTable::StripTable(std::string idetFile) : DetectorTable(idetFile) {
 		zonePitch = { {0.5f, 1.f}, {0.5f, 1.5f}, {0.5f, 0.5f}, {1.5f,1.f}, {1.5f, 1.5f}, {1.5f, 0.5f}, {1.f, 1.f}, {1.f, 1.5f}, {1.f, 0.5f} };
 		zoneInter = { {0.1f, 0.75f}, {0.1f, 1.12f}, {0.1f, 0.37f}, {0.1f,0.75f}, {0.1f, 1.12f}, {0.1f, 0.37f}, {0.1f, 0.75f}, {0.1f, 1.12f}, {0.1f, 0.37f} };
 	}
+	else if(idetFile.find("rd542") != std::string::npos){
+		zonePitch = { {1.2f, 1.f} };
+		zoneInter = { {0.125f, 0.7f} };
+	}
 	else{
-		throw std::runtime_error("Error: zones not recognized");
+		// throw std::runtime_error("Error: zones not recognized");
+		std::cout<<"Warning: detector zones not found, setting default pitch and inter values"<<std::endl;
+		zonePitch = { {1.2f, 1.f} };
+		zoneInter = { {0.125f, 0.7f} };
 	}
 }
 
