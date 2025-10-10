0b0abce8fc26efc452b06813f6a3d0ba287b4289
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2025-10-10 16:07:56 +0200

    json

--- recJSON/reco_det.cpp
+++ recJSON/reco_det.cpp
@@ -306,7 +306,7 @@ int main( int argc, char **argv ){
     // std::cout << "Added file " << f << std::endl;
   // }
   // ch->Add("../decode/ftest.root");
-  ch->Add("../../positionCut/dec_POS06_FEU1_inter.root");
+  ch->Add("../../positionCut/dec_POS06_FEU1_strip.root");
   std::cout << std::endl;
   std::cout << "connectors: " << pj.x1Dream() << ", " << pj.x2Dream() << ", " << pj.y1Dream() << ", " << pj.y2Dream() << std::endl;
 
